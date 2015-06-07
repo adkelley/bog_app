8d5bca33f2100a19d87372c34cc8750885e6b00a
Alex Kelley <adkelley@mac.com>
2015-06-07 14:47:47 -0700

    All routes, except PATCH:Update working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   root to: 'creatures#index'
-  ## Also just to keep it RESTful
-  get 'creatures', to: "creatures#index"
+  
+  ## Just to keep it RESTful
+  get '/creatures', to: "creatures#index"
+  get '/creatures/new', to: "creatures#new", as: "new_creature"
+  # rake routes to check this route out
+  get '/creatures/:id', to: "creatures#show", as: "creature"
+  get '/creatures/:id/edit', to: "creatures#edit", as: "edit_creature"
+  post '/creatures', to: "creatures#create"
+
+  # The update route
+  put "/creatures/:id/", to: "creatures#update"
+
+  # The destroy route
+  delete "/creatures/:id", to: "creatures#destroy"
+  
 end
